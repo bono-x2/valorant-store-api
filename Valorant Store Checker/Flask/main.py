64a3d0fb4d7ce4a6c32792d1179b9de4db5771d5
bono-x2 <165046612+bono-x2@users.noreply.github.com>
2024-04-04 17:20:46 +0900

    Update main.py

--- Valorant Store Checker/Flask/main.py
+++ Valorant Store Checker/Flask/main.py
@@ -11,6 +11,3 @@ def index():
 def get_store(username_api, password_api, region_api):
     valorant_store = ValorantStore(username=username_api, password=password_api, region=region_api, sess_path='./tmp', proxy=None)
     return render_template('store.html', everything=valorant_store.store(True))
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8090)
